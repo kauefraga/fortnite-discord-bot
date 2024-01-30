2cbe4547a76d79ddd0038c983813f9af61e72c3a
Kauê Fraga Rodrigues <kauefragarodrigues456@gmail.com>
2024-01-30 17:01:05 -0300

    feat: init discord session

--- cmd/main.go
+++ cmd/main.go
@@ -1,35 +1,24 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"os"
 	"os/signal"
 	"syscall"
 
-	"github.com/bwmarrin/discordgo"
-	"github.com/kauefraga/fortnite-discord-bot/internal/config"
+	"github.com/kauefraga/fortnite-discord-bot/internal/discord"
 )
 
 func main() {
-	env := config.GetEnv()
+	discord := discord.InitSession()
 
-	discord, err := discordgo.New("Bot " + env.DiscordAuthToken)
-
-	if err != nil {
-		panic(err)
-	}
-
-	discord.AddHandler(func(s *discordgo.Session, r *discordgo.Ready) {
-		fmt.Println("Bot is ready")
-	})
-
-	err = discord.Open()
+	err := discord.Open()
 
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Println("Bot is now running. Press CTRL-C to exit.")
+	log.Println("Bot is now running. Press CTRL-C to exit.")
 	sc := make(chan os.Signal, 1)
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
 	<-sc
